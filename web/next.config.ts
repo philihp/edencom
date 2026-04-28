d37021b583a8501894508b2da679e8f90372cefc
Philihp Busby <philihp@gmail.com>
2026-04-28 03:54:30 +0000

    tell next about packages

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['@edencom/character-slug'],
   images: {
     remotePatterns: [{ hostname: 'images.evetech.net' }],
   },
